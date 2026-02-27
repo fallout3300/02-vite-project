67a10e9b2b35f88fa637b49312acc4940bb33954
fallout3300 <polakovsasha@mail.ru>
2026-02-27 13:49:27 +0300

    исправляю пути в vite конфиге

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/02-vite-project/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
